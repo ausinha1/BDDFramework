5c8f162ec481b884c3633bdda1e3445caeb62118
ausinha1 <aurovindosinha@gmail.com>
2022-01-26 22:30:59 +0000

    added comment2

--- src/test/java/com/dellapp/stepdefinitions/TestRunner.java
+++ src/test/java/com/dellapp/stepdefinitions/TestRunner.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 //comment 1
+//comment 2
 @RunWith(Cucumber.class) 
 @CucumberOptions(features="Features",glue={"com.dellapp.stepdefinitions"},
 plugin = { "pretty","html:target/cucumber-reports/cucumber.html", "json:target/cucumber-reports/cucumber.json"},
